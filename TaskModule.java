72abb0c188147aafc5efdae00bc473355ca6f3b1
aliashraf6649 <113792656+aliashraf6649@users.noreply.github.com>
2023-12-09 20:21:13 +0200

    another updates

--- TaskModule.java
+++ TaskModule.java
@@ -56,7 +56,7 @@ public class TaskModule extends Module {
             }
 
         }
-        else if (!currentEmployee.empType.isManager)
+        else
         {
             System.out.println("\nEmpolyee\n");
             System.out.println("\nPick a choice: \n\n");
@@ -94,10 +94,79 @@ public class TaskModule extends Module {
     } 
     
     public void viewTasks() {
-        //TODO
+        
+        int len = Application.taskDataHandler.getLength();
+        System.out.println("Code\tTitle\n");
+        if(currentEmployee.empType.isManager)
+        {
+            for(int i = 0; i <len; i++){
+                Task task = Application.taskDataHandler.get(i);
+                if(task.project == project)
+                System.out.println(task.code + "\t" + task.title + "\n");
+            }
+        }
+        else
+        {
+            for(int i = 0; i <len; i++){
+                Task task = Application.taskDataHandler.get(i);
+                if(task.assignedEmployee == currentEmployee)
+                System.out.println(task.code + "\t" + task.title + "\n");
+            }
+        }
+        
+        boolean exit = false;
+        while (!exit)
+            {
+                System.out.println("\nPick a choice: \n\n");
+                System.out.println("1-View tasks \n");
+                System.out.println("2-Go Back \n");
+                int choice = Application.input.nextInt();
+                switch (choice)
+                {
+                    case 1:
+                        //TODO
+                        break;
+                    case 2:
+                        System.out.println("Going to the previous page");
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid choice, please try again.");
+                        break;
+                }
+            }
+
+        
     }
     public void viewTask(Task task) {
+
         //TODO
+
+        boolean exit = false;
+        while (!exit)
+            {
+                System.out.println("\nPick a choice: \n\n");
+                System.out.println("1-View tasklogs \n");
+                System.out.println("2-Create tasklog \n");
+                System.out.println("3-Go Back \n");
+                int choice = Application.input.nextInt();
+                switch (choice)
+                {
+                    case 1:
+                        viewTasklogs(task);
+                        break;
+                    case 2: //TODO
+                        createTaskLog(task, null, null); 
+                        break;
+                    case 3:
+                        System.out.println("Going to the previous page");
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid choice, please try again.");
+                        break;
+                }
+            }
     }
     public void viewTasklogs(Task task) {
         //TODO
