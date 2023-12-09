24db73e17e2f235487b77fcdbf2f35eed364cfe2
owm <enless143@gmail.com>
2023-12-09 11:47:59 +0200

    Added Application class (main entry point)

---
+++ Application.java
@@ -0,0 +1,32 @@
+public class Application
+{
+	DataHandler<User> userDataHandler;
+	DataHandler<Employee> employeeDataHandler;
+	DataHandler<Task> taskDataHandler;
+	DataHandler<LeaveRequest> leaveRequestDataHandler;
+	DataHandler<EmpType> empTypeDataHandler;
+	//DataHandler<TaskPhase> taskPhaseDataHandler;
+	DataHandler<Request> requestDataHandler;
+	DataHandler<TimeCard> timeCardDataHandler;
+	DataHandler<TaskLog> taskLogDataHandler;
+	DataHandler<Project> projectDataHandler;
+	Module currentModule;
+	User user;
+
+	public static void initializeData()
+	{
+		//TODO
+	}
+	public static void login()
+	{
+		//TODO
+	}
+	public static void startModule()
+	{
+		//TODO
+	}
+	public static void main(String[] args)
+	{
+		//TODO: start the project lol
+	}
+}
