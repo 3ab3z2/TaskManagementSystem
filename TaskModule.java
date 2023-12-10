e7cf7de2e5f67223c1d32b1fc387847a2f31801f
Omar Atya <omaratyamahboub@gmail.com>
2023-12-10 14:36:34 +0200

    all views on the TaskModule

--- TaskModule.java
+++ TaskModule.java
@@ -95,6 +95,7 @@ public class TaskModule extends Module {
     
     public void viewTasks() {
         
+        //display tasks
         int len = Application.taskDataHandler.getLength();
         System.out.println("Code\tTitle\n");
         if(currentEmployee.empType.isManager)
@@ -102,7 +103,7 @@ public class TaskModule extends Module {
             for(int i = 0; i <len; i++){
                 Task task = Application.taskDataHandler.get(i);
                 if(task.project == project)
-                System.out.println(task.code + "\t" + task.title + "\n");
+                    System.out.println(task.code + "\t" + task.title + "\n");
             }
         }
         else
@@ -110,21 +111,33 @@ public class TaskModule extends Module {
             for(int i = 0; i <len; i++){
                 Task task = Application.taskDataHandler.get(i);
                 if(task.assignedEmployee == currentEmployee)
-                System.out.println(task.code + "\t" + task.title + "\n");
+                    System.out.println(task.code + "\t" + task.title + "\n");
             }
         }
-        
+
+        //view tasks menu
         boolean exit = false;
         while (!exit)
             {
                 System.out.println("\nPick a choice: \n\n");
-                System.out.println("1-View tasks \n");
+                System.out.println("1-View task \n");
                 System.out.println("2-Go Back \n");
                 int choice = Application.input.nextInt();
                 switch (choice)
                 {
                     case 1:
-                        //TODO
+                    //TODO: error checking 
+                    String taskCodeInput = "";
+                    int taskCodeInputInt;
+                    System.out.println("please pick the task code that you wish to refer to: ");
+                    taskCodeInputInt = Application.input.nextInt();
+                    taskCodeInput = taskCodeInputInt + "";
+                    for(int i = 0; i < len; i++)
+                    {
+                        Task task = Application.taskDataHandler.get(i);
+                        if(taskCodeInput.equals(task.getCode()))
+                            viewTask(task);
+                    }
                         break;
                     case 2:
                         System.out.println("Going to the previous page");
@@ -140,8 +153,18 @@ public class TaskModule extends Module {
     }
     public void viewTask(Task task) {
 
-        //TODO
+        //display task details
+        System.out.println("Title: " + task.getTitle());
+        System.out.println("Code: " + task.getCode());
+        System.out.println("Description: " + task.getDescription());
+        System.out.println("Task phase: " + task.getTaskPhase());
+        System.out.println("Priority: " + task.getPriority().toString());
+        System.out.println("Assigned to: " + task.getAssignedEmployee().toString());
+        System.out.println("Start date: " + task.getStartDate().toString());
+        System.out.println("Due date: " + task.getEndDate().toString());
+        System.out.println("EST: " + task.getEST());
 
+        //display view task menu
         boolean exit = false;
         while (!exit)
             {
@@ -169,10 +192,47 @@ public class TaskModule extends Module {
             }
     }
     public void viewTasklogs(Task task) {
-        //TODO
-    }
+        //display all task logs
+        TaskLog[] arr = new TaskLog[Application.taskLogDataHandler.getLength()];
+        for(int i = 0, j =0; i < Application.taskLogDataHandler.getLength(); i++)
+        {
+            //check if == or equals
+            if(Application.taskLogDataHandler.get(i).getTask() == task)
+            {
+                System.out.println((i+1) + ".\nfromtime:" + Application.taskLogDataHandler.get(i).getFromTime().toString());
+                System.out.println("totime:" + Application.taskLogDataHandler.get(i).getToTime().toString());
+                arr[j] = Application.taskLogDataHandler.get(i);
+            }
+        }
+
+            
+            boolean exit = false;
+            while (!exit)
+            {
+                System.out.println("\nPick a choice: \n\n");
+                System.out.println("0-Go Back \n");
+                for (int i = 0; i < arr.length; i++) 
+                    System.out.println((i + 1) + "-\tFrom: " + arr[i].getFromTime().toString() + "\tTo:" + arr[i].getToTime().toString());
+                int choice = Application.input.nextInt();
+                
+                    if( choice == 0)
+                    {
+                        System.out.println("Going to the previous page");
+                        exit = true;
+                    }
+                    else if(choice >= arr.length || choice < 0)
+                        System.out.println("Invalid choice, please try again.");
+                    else
+                        for(int i = 0; i < arr.length; i++)
+                            if(i == choice - 1 )
+                                viewTasklog(arr[i]);
+            }
+        }    
     public void viewTasklog(TaskLog taskLog) {
-        //TODO
+        System.out.println("Task: "+ taskLog.getTask().toString());
+        System.out.println("From time:" + taskLog.getFromTime().toString());
+        System.out.println("To time:" + taskLog.getToTime().toString());
+        System.out.println("Assigned to:" + taskLog.getAssignedEmployee().toString());
     }
     public void manageTasks() {
         //TODO
